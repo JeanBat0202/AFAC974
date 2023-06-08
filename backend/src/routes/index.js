a0c57076d761af10f2d158c02d95a327daaa80b3
Julien Leroux <julienleroux9328@gmail.com>
2023-06-08 09:46:24 +0200

    dev fix3

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const artsRouter = require("./arts.routes");
 const authorsRouter = require("./authors.routes");
+const userRouter = require("./users.routes");
 
 router.use("/arts", artsRouter);
 router.use("/authors", authorsRouter);
+router.use("/users", userRouter);
 
 module.exports = router;
